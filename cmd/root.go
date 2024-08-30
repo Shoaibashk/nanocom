b1f0bb7379195ae1b9b6606b6227e783a982cfc0
shoaibashk <shoaibashk.2000@gmail.com>
2024-08-30 23:27:19 +0530

    Added CLI with port and baudrate

--- cmd/root.go
+++ cmd/root.go
@@ -4,9 +4,17 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
+	"go.bug.st/serial"
+)
+
+var (
+	baudrate int
+	port     string
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -17,7 +25,29 @@ var rootCmd = &cobra.Command{
 
 Used by developers in embedded systems and host machine to communicate serial devices.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
+		mode := &serial.Mode{
+			BaudRate: baudrate,
+		}
+		op, err := serial.Open(port, mode)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		buff := make([]byte, 100)
+		for {
+			n, err := op.Read(buff)
+			if err != nil {
+				log.Fatal(err)
+				break
+			}
+			if n == 0 {
+				fmt.Println("\nEOF")
+				break
+			}
+			fmt.Printf("%v", string(buff[:n]))
+		}
+		// cmd.Help()
+
 	},
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
@@ -35,11 +65,15 @@ func Execute() {
 
 func init() {
 
-	rootCmd.Flags().StringP("baudrate","b","COM2","set baudrate (ignore the value from config)")
+	rootCmd.Flags().IntVarP(&baudrate, "baudrate", "b", 9200, "set baudrate (ignore the value from config)")
+
+	// if err := rootCmd.MarkFlagRequired("baudrate"); err != nil {
+	// 	fmt.Println(err)
+	// }
+	rootCmd.Flags().StringVarP(&port, "port", "p", "COM2", "set port (ignore the value from config)")
+
 	// list := rootCmd.Flags().StringP("list","l","/dev/tty1","Get available list of serial port")
 
-
-	
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
@@ -50,5 +84,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
